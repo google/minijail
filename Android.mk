8b330bf3b28f6d1cf40852c612affbbd2aff8a28
Jorge Lucangeli Obes <jorgelo@google.com>
2015-08-04 10:42:05 -0700

    minijail: Export include dir.
    Bug: 22555701
    Change-Id: I2b6926e32bded394f88c4cb6bffcdf0d1a15e625

--- Android.mk
+++ Android.mk
@@ -43,6 +43,8 @@ LOCAL_SRC_FILES := \
 	libminijail.c \
 	signal_handler.c \
 	syscall_filter.c \
-	util.c
+	util.c \
+
 LOCAL_SHARED_LIBRARIES := $(minijailCommonSharedLibraries)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 include $(BUILD_SHARED_LIBRARY)
