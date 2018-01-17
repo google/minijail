8f0665b0cebe1d459783ae5b65adec03447f073b
Mike Frysinger <vapier@google.com>
2018-01-17 14:26:09 -0500

    make the dest field with -b optional
    Often times with -b, you want to bind mount the source into the same
    location (like when creating chroots), so you have a lot of flags like:
      -b /var,/var
      -b /dev/log,/dev/log
    
    Make the destination default to the source so now these are equivalent:
      -b /var
      -b /dev/log
    
    Bug: None
    Test: `sudo ./minijail0 --profile minimalistic-mountns -b /dev/log /usr/bin/logger foo` works
    Change-Id: I4dcf22c1d58e68535c3542d10f80974561d680c5

--- minijail0.c
+++ minijail0.c
@@ -95,10 +95,12 @@ static void add_binding(struct minijail *j, char *arg)
 	char *src = strtok(arg, ",");
 	char *dest = strtok(NULL, ",");
 	char *flags = strtok(NULL, ",");
-	if (!src || !dest) {
+	if (!src) {
 		fprintf(stderr, "Bad binding: %s %s\n", src, dest);
 		exit(1);
 	}
+	if (!dest)
+		dest = src;
 	if (minijail_bind(j, src, dest, flags ? atoi(flags) : 0)) {
 		fprintf(stderr, "minijail_bind failed.\n");
 		exit(1);
@@ -294,7 +296,7 @@ static void usage(const char *progn)
 	/* clang-format off */
 	printf("Usage: %s [-dGhHiIKlLnNprRstUvyYz]\n"
 	       "  [-a <table>]\n"
-	       "  [-b <src>,<dest>[,<writeable>]] [-k <src>,<dest>,<type>[,<flags>][,<data>]]\n"
+	       "  [-b <src>[,<dest>[,<writeable>]]] [-k <src>,<dest>,<type>[,<flags>][,<data>]]\n"
 	       "  [-c <caps>] [-C <dir>] [-P <dir>] [-e[file]] [-f <file>] [-g <group>]\n"
 	       "  [-m[<uid> <loweruid> <count>]*] [-M[<gid> <lowergid> <count>]*] [--profile <name>]\n"
 	       "  [-R <type,cur,max>] [-S <file>] [-t[size]] [-T <type>] [-u <user>] [-V <file>]\n"
