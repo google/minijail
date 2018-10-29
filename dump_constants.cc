f7b201804bb5e285066b76c6804f498e4b77ea22
Luis Hector Chavez <lhchavez@google.com>
2018-10-28 21:39:32 -0700

    dump_constants: Add a tool to generate a JSON file with all constants
    This change adds a way to export all the necessary constants so that
    external tools can compile the policy files.
    
    Bug: chromium:856315
    Test: make all
    
    Change-Id: I8637c63b9b09839616aa66ef1714444cb4e124ee

---
+++ dump_constants.cc
@@ -0,0 +1,46 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// Generate a .json file with all the architecture-specific constants.
+
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include "arch.h"
+#include "libconstants.h"
+#include "libsyscalls.h"
+
+int main() {
+  std::cout << "{\n";
+  std::cout << "  \"arch_nr\": " << ARCH_NR << ",\n";
+  std::cout << "  \"bits\": " << (sizeof(uintptr_t) * 8) << ",\n";
+  std::cout << "  \"syscalls\": {\n";
+  bool first = true;
+  for (const struct syscall_entry* entry = syscall_table; entry->name;
+       ++entry) {
+    if (first)
+      first = false;
+    else
+      std::cout << ",\n";
+    std::cout << "    \"" << entry->name << "\": " << entry->nr;
+  }
+  std::cout << "\n  },\n";
+  std::cout << "  \"constants\": {\n";
+  first = true;
+  for (const struct constant_entry* entry = constant_table; entry->name;
+       ++entry) {
+    if (first)
+      first = false;
+    else
+      std::cout << ",\n";
+    std::cout << "    \"" << entry->name << "\": " << entry->value;
+  }
+  std::cout << "\n  }\n";
+  std::cout << "}\n";
+
+  return 0;
+}
