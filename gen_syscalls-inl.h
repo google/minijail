3b58ccb3072c5908c79d65339e886b344f49c5d1
Alistair Delva <adelva@google.com>
2020-08-24 13:54:16 -0700

    Workarounds for Android host glibc toolchain
    When minijail is built against the Android host glibc toolchain, its
    syscall and ioctl coverage becomes limited by the very old Linux C
    headers the toolchain is using. Because crosvm jails subprocesses
    that can load e.g. GL libraries or FUSE which may be built with much
    newer glibc versions, we need support for some newer syscalls and
    ioctls added to Linux.
    
    Minijail will throw parse errors for any syscall or ioctl in .policy
    files that it doesn't understand; and anyway, it wouldn't be meaningful
    to strip these, as .policy files are inclusion (not exclusion) based.
    
    This change isn't very nice, but it does unblock us from running crosvm
    built by the Android host toolchain with sandboxing enabled.
    
    Change-Id: Iab7f2e7abac0f5e154e300833b8d91d7b8500aff

---
+++ gen_syscalls-inl.h
@@ -0,0 +1,63 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <asm/unistd.h>
+
+/* Ideally minijail is compiled against a modern libc, which has modern copies
+ * of Linux uapi for ioctls, and unistd.h for syscalls. However, sometimes this
+ * isn't possible - such as when building with the Android host toolchain - so
+ * locally define the system calls in use in active seccomp policy files.
+ * This UAPI is taken from sanitized bionic headers.
+ */
+
+#ifndef __NR_copy_file_range
+#ifdef __x86_64__
+#define __NR_copy_file_range 326
+#elif __i386__
+#define __NR_copy_file_range 377
+#elif __arm64__
+#define __NR_copy_file_range 285
+#endif
+#endif /* __NR_copy_file_range */
+
+#ifndef __NR_getrandom
+#ifdef __x86_64__
+#define __NR_getrandom 318
+#elif __i386__
+#define __NR_getrandom 355
+#elif __arm64__
+#define __NR_getrandom 278
+#endif
+#endif /* __NR_getrandom */
+
+#ifndef __NR_memfd_create
+#ifdef __x86_64__
+#define __NR_memfd_create 319
+#elif __i386__
+#define __NR_memfd_create 356
+#elif __arm64__
+#define __NR_memfd_create 279
+#endif
+#endif /* __NR_memfd_create */
+
+#ifndef __NR_renameat2
+#ifdef __x86_64__
+#define __NR_renameat2 316
+#elif __i386__
+#define __NR_renameat2 353
+#elif __arm64__
+#define __NR_renameat2 276
+#endif
+#endif /* __NR_renameat2 */
+
+#ifndef __NR_statx
+#ifdef __x86_64__
+#define __NR_statx 332
+#elif __i386__
+#define __NR_statx 383
+#elif __arm64__
+#define __NR_statx 291
+#endif
+#endif /* __NR_statx */
