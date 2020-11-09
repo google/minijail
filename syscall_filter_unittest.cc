aba39460ab0f216c772a237342b534b8e4049e3d
Nicole Anderson-Au <nvaa@google.com>
2020-11-09 23:36:56 +0000

    minijail: Add tests to check for duplicate syscall definitions
    Add more tests to syscall_filter_unittest that check various
    combinations of syscall definitions with and without arguments and with
    and without allowing duplicate syscalls.
    
    Bug: chromium:1146502
    TEST=built and ran unit tests
    
    Change-Id: I698d52da8da6e3b7edcf6ede11db535581af2830

--- syscall_filter_unittest.cc
+++ syscall_filter_unittest.cc
@@ -43,21 +43,18 @@ enum use_logging {
   USE_RET_LOG_LOGGING = 2,
 };
 
-/*
- * TODO(crbug.com/1146502) Add more tests for greater granularity for checking
- * for duplicate syscalls.
- */
 int test_compile_filter(
     std::string filename,
     FILE* policy_file,
     struct sock_fprog* prog,
     enum block_action action = ACTION_RET_KILL,
-    enum use_logging allow_logging = NO_LOGGING) {
+    enum use_logging allow_logging = NO_LOGGING,
+    bool allow_dup_syscalls = true) {
   struct filter_options filteropts {
     .action = action,
     .allow_logging = allow_logging != NO_LOGGING,
     .allow_syscalls_for_logging = allow_logging == USE_SIGSYS_LOGGING,
-    .allow_duplicate_syscalls = true,
+    .allow_duplicate_syscalls = allow_dup_syscalls,
   };
   return compile_filter(filename.c_str(), policy_file, prog, &filteropts);
 }
@@ -70,12 +67,13 @@ int test_compile_file(
     struct bpf_labels* labels,
     enum block_action action = ACTION_RET_KILL,
     enum use_logging allow_logging = NO_LOGGING,
-    unsigned int include_level = 0) {
+    unsigned int include_level = 0,
+    bool allow_dup_syscalls = false) {
   struct filter_options filteropts {
     .action = action,
     .allow_logging = allow_logging != NO_LOGGING,
     .allow_syscalls_for_logging = allow_logging == USE_SIGSYS_LOGGING,
-    .allow_duplicate_syscalls = true,
+    .allow_duplicate_syscalls = allow_dup_syscalls,
   };
   size_t num_syscalls = get_num_syscalls();
   struct parser_state **previous_syscalls =
@@ -1322,6 +1320,90 @@ TEST(FilterTest, seccomp_mode1) {
   free(actual.filter);
 }
 
+TEST(FilterTest, seccomp_mode1_with_check) {
+  struct sock_fprog actual;
+  std::string policy =
+      "read: 1\n"
+      "write: 1\n"
+      "rt_sigreturn: 1\n"
+      "exit: 1\n";
+
+  FILE* policy_file = write_policy_to_pipe(policy);
+  ASSERT_NE(policy_file, nullptr);
+
+  int res = test_compile_filter("policy", policy_file, &actual,
+                                ACTION_RET_KILL, NO_LOGGING, false
+                                /* allow duplicate syscalls */);
+  fclose(policy_file);
+
+  /*
+   * Checks return value, filter length, and that the filter
+   * validates arch, loads syscall number, and
+   * only allows expected syscalls.
+   */
+  ASSERT_EQ(res, 0);
+  EXPECT_EQ(actual.len, 13);
+  EXPECT_ARCH_VALIDATION(actual.filter);
+  EXPECT_EQ_STMT(actual.filter + ARCH_VALIDATION_LEN,
+                 BPF_LD + BPF_W + BPF_ABS,
+                 syscall_nr);
+  EXPECT_ALLOW_SYSCALL(actual.filter + ARCH_VALIDATION_LEN + 1, __NR_read);
+  EXPECT_ALLOW_SYSCALL(actual.filter + ARCH_VALIDATION_LEN + 3, __NR_write);
+  EXPECT_ALLOW_SYSCALL(actual.filter + ARCH_VALIDATION_LEN + 5,
+                       __NR_rt_sigreturn);
+  EXPECT_ALLOW_SYSCALL(actual.filter + ARCH_VALIDATION_LEN + 7, __NR_exit);
+  EXPECT_EQ_STMT(actual.filter + ARCH_VALIDATION_LEN + 9,
+                 BPF_RET + BPF_K,
+                 SECCOMP_RET_KILL);
+
+  free(actual.filter);
+}
+
+/*
+ * This fails even with allow_duplicate_syscalls set to true because the
+ * creation of labels for the arguments causes conflicts which cause the
+ * compile_filter function to fail.
+ */
+TEST(FilterTest, duplicate_read_with_args) {
+  struct sock_fprog actual;
+  std::string policy =
+      "read: arg0 == 0\n"
+      "read: arg1 == 1\n"
+      "write: 1\n"
+      "rt_sigreturn: 1\n"
+      "exit: 1\n";
+
+  FILE* policy_file = write_policy_to_pipe(policy);
+  ASSERT_NE(policy_file, nullptr);
+  int res = test_compile_filter("policy", policy_file, &actual);
+  fclose(policy_file);
+
+  ASSERT_EQ(res, -1);
+}
+
+/*
+ * This does not fail because only one instance of read defines an argument.
+ */
+TEST(FilterTest, duplicate_read_with_one_arg) {
+  struct sock_fprog actual;
+  std::string policy =
+      "read: arg0 == 0\n"
+      "read: 1\n"
+      "write: 1\n"
+      "rt_sigreturn: 1\n"
+      "exit: 1\n";
+
+  FILE* policy_file = write_policy_to_pipe(policy);
+  ASSERT_NE(policy_file, nullptr);
+  int res = test_compile_filter("policy", policy_file, &actual);
+  fclose(policy_file);
+
+  /* TODO: Don't know how to generate a correct value to validate the filter
+   * that is generated. */
+  ASSERT_EQ(res, 0);
+  free(actual.filter);
+}
+
 TEST(FilterTest, seccomp_mode1_trap) {
   struct sock_fprog actual;
   std::string policy =
@@ -1878,6 +1960,26 @@ TEST(FilterTest, include_same_syscalls) {
             ARCH_VALIDATION_LEN + 1 /* load syscall nr */ +
                 2 * 8 /* check syscalls twice */ + 1 /* filter return */);
   free(actual.filter);
+}
+
+TEST(FilterTest, include_same_syscalls_with_check) {
+  struct sock_fprog actual;
+  std::string policy =
+      "read: 1\n"
+      "write: 1\n"
+      "rt_sigreturn: 1\n"
+      "exit: 1\n"
+      "@include " + source_path("test/seccomp.policy") + "\n";
+
+  FILE* policy_file = write_policy_to_pipe(policy);
+  ASSERT_NE(policy_file, nullptr);
+
+  int res = test_compile_filter("policy", policy_file, &actual,
+                                ACTION_RET_KILL, NO_LOGGING, false
+                                /* allow duplicate syscalls */);
+  fclose(policy_file);
+
+  ASSERT_EQ(res, -1);
 }
 
 TEST(FilterTest, include_two) {
