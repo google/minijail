4c33189fa48ba225ad4e9e2133020ea5f52b5486
Mike Frysinger <vapier@chromium.org>
2022-09-13 05:17:08 -0400

    Update license boilerplate text in source code files
    Normally we don't do this, but enough changes have accumulated that
    we're doing a tree-wide one-off update of the name & style.
    
    BUG=chromium:1098010
    TEST=`repo upload` works
    
    Change-Id: I208569250e49179362ec9e2343ffc13652f576dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/minijail/+/3893890
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Auto-Submit: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Allen Webb <allenwebb@google.com>

--- libminijailpreload.c
+++ libminijailpreload.c
@@ -1,5 +1,5 @@
 /* libminijailpreload.c - preload hack library
- * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Copyright 2011 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
