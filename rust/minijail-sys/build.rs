90e7912ac45bc84a7f7f773bfaa26b5ad695cb91
Allen Webb <allenwebb@google.com>
2022-10-24 19:41:56 +0000

    minijail-sys: Refactor to always generate bindings via crate.
    Notable changes:
    * The sources generated by bindgen are no longer checked in.
    * The bindgen crate is used instead of the executable.
    * The bindings are written to ${OUT_DIR} instead of the source tree.
    
    BUG=b:231346184,chromium:1032672
    TEST=CQ passes + cargo build doesn't result in a dirty tree.
    
    Cq-Depend: chromium:3978191
    Change-Id: I0b2388b8e3718bdef6b3cd471fe1ca1107041ae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/minijail/+/3975851
    Reviewed-by: Victor Hsieh <victorhsieh@chromium.org>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Tested-by: Allen Webb <allenwebb@google.com>

--- rust/minijail-sys/build.rs
+++ rust/minijail-sys/build.rs
@@ -7,10 +7,14 @@
 /// This script prefers linking against a pkg-config provided libminijail, but will fall back to
 /// building libminijail statically.
 use std::env;
-use std::fs::remove_file;
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::Command;
+
+use bindgen::EnumVariation;
+
+const HEADER_FILENAME: &str = "libminijail.h";
+const OUT_FILENAME: &str = "libminijail.rs";
 
 /// Returns the target triplet prefix for gcc commands. No prefix is required
 /// for native builds.
@@ -35,13 +39,18 @@ fn get_cross_compile_prefix() -> String {
     return format!("{}-{}-{}-", arch, os, env);
 }
 
-fn set_up_libminijail() -> io::Result<()> {
+fn set_up_libminijail() -> io::Result<PathBuf> {
     // Minijail requires libcap at runtime.
     pkg_config::Config::new().probe("libcap").unwrap();
 
     // Prefer a system-provided Minijail library.
-    if pkg_config::Config::new().probe("libminijail").is_ok() {
-        return Ok(());
+    if let Ok(info) = pkg_config::Config::new().probe("libminijail") {
+        for path in info.include_paths {
+            let header_path = path.join(HEADER_FILENAME);
+            if header_path.exists() {
+                return Ok(header_path);
+            }
+        }
     }
 
     let current_dir = env::var("CARGO_MANIFEST_DIR").unwrap() + "/../..";
@@ -62,56 +71,44 @@ fn set_up_libminijail() -> io::Result<()> {
     }
     println!("cargo:rustc-link-search=native={}", &out_dir);
     println!("cargo:rustc-link-lib=static=minijail.pic");
-    Ok(())
+
+    let header_dir = Path::new("../../");
+    let header_path = header_dir.join(HEADER_FILENAME);
+    Ok(header_path)
 }
 
-fn bindings_generation() -> io::Result<()> {
-    let bindgen = match which::which("bindgen") {
-        Ok(v) => v,
-        // Use already generated copy if bindgen is not present.
-        _ => return Ok(()),
-    };
+fn bindings_generation(header_path: &str) -> io::Result<()> {
+    println!("cargo:rerun-if-changed={}", header_path);
+    println!("cargo:rerun-if-changed=build.rs");
 
-    // If CROS_RUST is set, skip generation.
-    let gen_file = Path::new("./libminijail.rs");
-    if gen_file.exists() {
-        if env::var("CROS_RUST") == Ok(String::from("1")) {
-            return Ok(());
-        }
-        remove_file(gen_file).expect("Failed to remove generated file.");
-    }
-    let header_dir = Path::new("../../");
-    let header_path = header_dir.join("libminijail.h");
-    println!("cargo:rerun-if-changed={}", header_path.display());
-    let status = Command::new(&bindgen)
-        .args(&["--default-enum-style", "rust"])
-        .args(&["--blacklist-type", "__rlim64_t"])
-        .args(&["--raw-line", "pub type __rlim64_t = u64;"])
-        .args(&["--blacklist-type", "__u\\d{1,2}"])
-        .args(&["--raw-line", "pub type __u8 = u8;"])
-        .args(&["--raw-line", "pub type __u16 = u16;"])
-        .args(&["--raw-line", "pub type __u32 = u32;"])
-        .args(&["--blacklist-type", "__uint64_t"])
-        .args(&["--whitelist-function", "^minijail_.*"])
-        .args(&["--whitelist-var", "^MINIJAIL_.*"])
-        .arg("--size_t-is-usize")
-        .arg("--no-layout-tests")
-        .arg("--disable-header-comment")
-        .args(&["--output", gen_file.to_str().unwrap()])
-        .arg(header_path.to_str().unwrap())
-        .args(&[
-            "--",
-            "-DUSE_BINDGEN",
-            "-D_FILE_OFFSET_BITS=64",
-            "-D_LARGEFILE_SOURCE",
-            "-D_LARGEFILE64_SOURCE",
-        ])
-        .status()?;
-    assert!(status.success());
-    Ok(())
+    let bindings = bindgen::Builder::default()
+        .header(header_path)
+        .default_enum_style(EnumVariation::Rust {
+            non_exhaustive: false,
+        })
+        .blocklist_type("__rlim64_t")
+        .raw_line("pub type __rlim64_t = u64;")
+        .blocklist_type("__u\\d{1,2}")
+        .raw_line("pub type __u8 = u8;")
+        .raw_line("pub type __u16 = u16;")
+        .raw_line("pub type __u32 = u32;")
+        .blocklist_type("__uint64_t")
+        .allowlist_function("^minijail_.*")
+        .allowlist_var("^MINIJAIL_.*")
+        .size_t_is_usize(true)
+        .layout_tests(false)
+        .disable_header_comment()
+        .clang_arg("-DUSE_BINDGEN")
+        .clang_arg("-D_FILE_OFFSET_BITS=64")
+        .clang_arg("-D_LARGEFILE_SOURCE")
+        .clang_arg("-D_LARGEFILE64_SOURCE")
+        .generate()
+        .expect("failed to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(out_path.join(OUT_FILENAME))
 }
 
 fn main() -> io::Result<()> {
-    set_up_libminijail()?;
-    bindings_generation()
+    let header_path = set_up_libminijail()?;
+    bindings_generation(header_path.to_str().unwrap())
 }
